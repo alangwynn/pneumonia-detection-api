ff8c776ad7f047cf19e0f1f94b7417e5a82b80ac
alangwynn <alan.gwynn1999@gmail.com>
2024-04-13 00:22:46 -0400

    add model from data path

--- src/services/ScanImagenService.py
+++ src/services/ScanImagenService.py
@@ -1,4 +1,3 @@
-
 from src.utils.Logger import Logger
 import tensorflow as tf
 from keras.preprocessing import image
@@ -7,13 +6,18 @@ import numpy as np
 class ScanImageService():
 
     @classmethod
-    def image_prediction(cls, new_image_path):
-        test_image = image.load_img(new_image_path, target_size = (224, 224))
+    def image_prediction(cls, image_path):
+        test_image = image.load_img(image_path, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
+        test_image = np.expand_dims(test_image, axis=0)
         test_image = test_image / 255.0
-        model_loaded = tf.keras.models.load_model("/kaggle/working/my_pneumonia_detection_model.h5")
+        
+        model_path = "data/models/pneumonia_detection_model.h5"
+        
+        model_loaded = tf.keras.models.load_model(model_path)
+        
         prediction = model_loaded.predict(test_image)
+        
         if prediction[0] > 0.5:
             statistic = prediction[0] * 100 
             return statistic, "PNEUMONIA"
